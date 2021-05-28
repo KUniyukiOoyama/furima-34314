a3f2b9f1af0de3dcc34fd2c3238d8efe0a5d338a
KUniyukiOoyama <79424305+KUniyukiOoyama@users.noreply.github.com>
2021-05-28 14:43:16 +0900

    ユーザー登録機能導入

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,16 @@
+class ItemsController < ApplicationController
+  def index
+  end
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to root_path
+  end
+
+  def new
+    @item = Item.new
+  end
+
+
+
+end
