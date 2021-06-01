08eae5f8efc818a47b227c604ea0097ce79416bb
KUniyukiOoyama <79424305+KUniyukiOoyama@users.noreply.github.com>
2021-06-01 10:22:29 +0900

    ATIVEHASHの付与

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,13 +2,17 @@ class ItemsController < ApplicationController
    def index
    end
 
-  # def destroy
-  #   @item = Item.find(params[:id])
-  #   @item.destroy
-  #   redirect_to root_path
-  # end
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to root_path
+  end
 
-  # def new
-  #   @item = Item.new
-  # end
+  def new
+    @item = Item.new
+    @items = Item.order("created_at DESC")
+
+
+
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,26 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :shipping
+  belongs_to :status
+  belongs_to :category
+  belongs_to :lead_time
+  belongs_to :prefecture
+
+
+  with_options numericality: { other_than: 1 }  do
+    validates :shipping_id
+    validates :status_id
+    validates :category_id
+    validates :lead_time_id
+    validates :prefecture_id
+  end
+
+  with_options presence: true do
+    validates :title
+    validates :description 
+    validates :price
+  end
+
 end
