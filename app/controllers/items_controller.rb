6eff0678e80be6073ee80a8422924c690c814484
KUniyukiOoyama <79424305+KUniyukiOoyama@users.noreply.github.com>
2021-06-08 19:10:32 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index,:show]
   def index
-
     @items = Item.all.order(created_at: "DESC")
-
   end
 
   # def destroy
@@ -23,7 +21,12 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
 
+
+  def show
+    @item = Item.find(params[:id])
+   
   end
 
 
