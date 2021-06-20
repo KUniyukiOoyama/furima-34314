da499217afc2c01ffc941068c6687508f938f4f6
KUniyukiOoyama <79424305+KUniyukiOoyama@users.noreply.github.com>
2021-06-21 07:14:12 +0900

    PAYJP決済機能とテストコード

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,16 @@
 class OrdersController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
+      return redirect_to root_path
     else
       render :index
     end
@@ -16,8 +20,13 @@ end
 
 private
 def order_params
-  params.require(:order_address).permit(:prefecture_id,:city,:house_number,:building_name,:post_number,:phone_number).merge(user_id: current_user.id,item_id: params[:item_id])
+  params.require(:order_address).permit(:prefecture_id,:city,:house_number,:building_name,:post_number,:phone_number).merge(user_id: current_user.id,item_id: params[:item_id],token: params[:token])
 end
-# def adress_params
-#   params.permit(:prefecture_id,:city,:house_number,:building_name,:post_number,:phone_number).merge(user_id: current_user.id)
-# end
+def pay_item
+  Payjp.api_key = "sk_test_b7da9a804c45eca50788ab8c"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: @item.price ,  # 商品の値段
+        card: order_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
+end
---
+++ spec/models/order_address_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+RSpec.describe OrderAddress, type: :model do
+  describe '商品購入機能' do
+    before do
+      @order_address = FactoryBot.build(:order_address)
+      sleep(1)
+    end
+
+    context'商品が購入できる時'do
+      it 'token,post_number,prefecture_id,post_number,phone_number,city,house_numberがあれば登録できる' do
+       expect(@order_address).to be_valid
+      end
+    end
+    
+    context'商品が購入できない時' do
+      it 'tokenが空の時登録できない' do
+        @order_address.token = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Token can't be blank")
+      end
+      it 'prefecture_idが空の時登録できない' do
+        @order_address.prefecture_id = 1
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+      it 'post_numberが空の時登録できない' do
+        @order_address.post_number = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Post number can't be blank")
+      end
+      it 'cityが空の時登録できない' do
+        @order_address.city = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("City can't be blank")
+      end
+      it 'house_numberが空の時登録できない' do
+        @order_address.house_number = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("House number can't be blank")
+      end
+      it 'phone_numberが空の時登録できない' do
+        @order_address.phone_number = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it 'post_numberは-がないと登録できない' do
+        @order_address.post_number = '1234567'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Post number is invalid")
+      end
+      it 'post_numberは半角英数字混合では登録できない' do
+        @order_address.post_number = '123-45aa'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Post number is invalid")
+      end
+      it 'phone_numberに文字列があると登録ができない' do
+        @order_address.phone_number = '0901234567a'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+      end
+      it 'phone_numberが11桁以上の数値だと登録ができない' do
+        @order_address.phone_number = '090111111111'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+      end
+    end
+
+  end
+end
